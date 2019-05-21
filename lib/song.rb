a0b6c95eddb593b0cda2feeb3f3100df96aff553
Meg Galvez <meg@oakmaine.com>
2019-05-21 19:16:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   
   def artist=(name)
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    artist.add_song(self) unless artist.
   end
   
   def artist
