f72b741240431904f6ec932cb7af569431b0bda5
Meg Galvez <meg@oakmaine.com>
2019-05-21 19:11:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,8 +26,7 @@ class Song
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
-    #binding.pry 
-    @artist.name = song_info[0] unless artist.songs == song 
+    song.artist=(song_info[0])
     song
   end
 
