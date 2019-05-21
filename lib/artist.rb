4865e5733bfae517c08112446baa2ed5ab4aa6ed
Meg Galvez <meg@oakmaine.com>
2019-05-21 19:11:12 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,51 @@
+require 'pry'
+
+class Artist 
+  
+  attr_accessor :name, :songs  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self 
+  end 
+  
+  def self.find_or_create_by_name(name)
+    if self.find(name)
+      self.find(name)
+    else
+      self.create(name)
+    end
+  end
+
+   def self.find(name)    
+    self.all.detect { |artist| artist.name == name }
+  end 
+  
+  def self.create(name)   
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self unless song.artist == self 
+  end
+  
+  def save
+    @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end
+  
+  def print_songs
+    self.songs.each do |song|
+      puts song.name
+    end 
+  end 
+  
+end 
---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+require 'pry'
+
+class Song 
+  
+  attr_accessor :name
+  attr_reader :artist  
+  @@all = [] 
+  
+  def initialize(name)
+    @name = name 
+  end 
+  
+  def name=(name)
+    @name = name 
+  end 
+  
+  def artist=(artist)
+    @artist = artist
+    
+    #binding.pry 
+  end
+  def artist
+    @artist
+  end 
+  
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info[1])
+    #binding.pry 
+    @artist.name = song_info[0] unless artist.songs == song 
+    song
+  end
+
+  
+   
+end 
