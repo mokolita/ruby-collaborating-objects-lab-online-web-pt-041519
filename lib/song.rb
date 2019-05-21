bdd953409e4e3412f1e89076149125305bbf28e9
Meg Galvez <meg@oakmaine.com>
2019-05-21 19:12:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
     song_info = file.chomp(".mp3").split(" - ")
     song = Song.new(song_info[1])
     song.artist=(song_info[0])
-    binding.pry   
+    binding.pry    
     song
   end
 
